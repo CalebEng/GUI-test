e260d2ec0b67ddde17af9ee09e492a22a83a9de7
CalebEng <121829627+CalebEng@users.noreply.github.com>
2023-01-10 16:16:50 -0500

    Re worked panel set up

--- GUI.java
+++ GUI.java
@@ -4,13 +4,6 @@
  * Gui object settings for use
  */
 
-
-
-
-
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
-import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -21,16 +14,18 @@ public class GUI{
         JPanel panel = new JPanel();
 
         //default size of the panel
-        panel.setBorder(BorderFactory.createEmptyBorder(720, 1080, 100, 100));
-        panel.setLayout(new GridLayout(0, 1));
+        frame.setSize(1080, 720);
 
 
         //setup for the GUI panel
-        frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("BlackJack-Frame-Test");
-        frame.pack();
         frame.setVisible(true);
+        frame.add(panel);
+
+        panel.setLayout(null);
+        
+        frame.setTitle("BlackJack-Frame-Test");
+        
     }   
 }
 
