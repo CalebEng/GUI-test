22bd5244558ac179f27a2aaafa04ba0a76834b1c
CalebEng <121829627+CalebEng@users.noreply.github.com>
2023-01-10 16:55:22 -0500

    experimenting with Javax springlayout

--- GUI.java
+++ GUI.java
@@ -4,28 +4,37 @@
  * Gui object settings for use
  */
 
+import java.awt.Dimension;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.SpringLayout;
 
 public class GUI{
 
     public GUI(){
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
+        SpringLayout layout = new SpringLayout();
 
         //default size of the panel
+        frame.setMinimumSize(new Dimension(1080,720));
         frame.setSize(1080, 720);
 
 
         //setup for the GUI panel
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        
         frame.add(panel);
 
         panel.setLayout(null);
         
         frame.setTitle("BlackJack-Frame-Test");
-        
+
+        JLabel name = new JLabel("Player name:", SwingConstants.CENTER);
+        panel.add(name);
+        frame.setVisible(true);
     }   
 }
 
